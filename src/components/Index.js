2aff7d0b392178ee20471129ee7d09abd00be14b
Ligita <legyta@gmail.com>
2021-03-25 09:08:41 +0100

    take offline fully

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,70 +1,70 @@
-import React from "react";
-import "../stylesheets/index.scss";
-import { Link } from "react-router-dom";
-import NavBar from "./NavBar";
-import Footer from "./Footer";
-import leaf from "../images/leaf.svg";
-import beach from "../images/beach.JPG";
-import { Component } from "react";
-import evaThree from "../images/evaThree.JPG";
+// import React from "react";
+// import "../stylesheets/index.scss";
+// import { Link } from "react-router-dom";
+// import NavBar from "./NavBar";
+// import Footer from "./Footer";
+// import leaf from "../images/leaf.svg";
+// import beach from "../images/beach.JPG";
+// import { Component } from "react";
+// import evaThree from "../images/evaThree.JPG";
 
-function Index() {
-  return (
-    <div className="index-page">
-      <Link to="home"></Link>
-      <NavBar />
-      <div className="index-title">
-        <div className="index-format">
-          <div className="index-image-eva">
-            {" "}
-            <img src={evaThree} alt="eva" />
-          </div>
+// function Index() {
+//   return (
+//     <div className="index-page">
+//       <Link to="home"></Link>
+//       <NavBar />
+//       <div className="index-title">
+//         <div className="index-format">
+//           <div className="index-image-eva">
+//             {" "}
+//             <img src={evaThree} alt="eva" />
+//           </div>
 
-          <div className="index-name-title">
-            <h1>Eva Serra</h1>
-            <h2>Integrative psychotherapist</h2>
+//           <div className="index-name-title">
+//             <h1>Eva Serra</h1>
+//             <h2>Integrative psychotherapist</h2>
 
-            <h2>BACP & UKCP Acredited/Registered</h2>
-            <h2>Currently studying</h2>
-          </div>
-        </div>
+//             <h2>BACP & UKCP Acredited/Registered</h2>
+//             <h2>Currently studying</h2>
+//           </div>
+//         </div>
 
-        <div className="index-image-leaf">
-          {/* <div className="index-image">
-            <img src={leaf} alt="leaf" />
-          </div> */}
+//         <div className="index-image-leaf">
+//           {/* <div className="index-image">
+//             <img src={leaf} alt="leaf" />
+//           </div> */}
 
-          <div className="index-image-beach">
-            <img src={beach} alt="beach" />
-          </div>
+//           <div className="index-image-beach">
+//             <img src={beach} alt="beach" />
+//           </div>
 
-          {/* <div className="index-image">
-            <img src={leaf} alt="leaf" />
-          </div> */}
-        </div>
+//           {/* <div className="index-image">
+//             <img src={leaf} alt="leaf" />
+//           </div> */}
+//         </div>
 
-        <div className="beach-title">
-          <h2>Remembering the home within us</h2>
-        </div>
+//         <div className="beach-title">
+//           <h2>Remembering the home within us</h2>
+//         </div>
 
-        <div className="index-image">
-          <img src={leaf} alt="leaf" />
-        </div>
-        {/* <div className="button-clients">
-          <button className="button">
-            <Link to="contact">Make an appointment</Link>
-          </button>
-        </div> */}
+//         <div className="index-image">
+//           <img src={leaf} alt="leaf" />
+//         </div>
+//         {/* <div className="button-clients">
+//           <button className="button">
+//             <Link to="contact">Make an appointment</Link>
+//           </button>
+//         </div> */}
 
-        {/* <div className="index-image">
-          <img src={leaf} alt="leaf" />
-        </div> */}
-      </div>
-      <div className="index-footer">
-        <Footer />
-      </div>
-    </div>
-  );
-}
+//         {/* <div className="index-image">
+//           <img src={leaf} alt="leaf" />
+//         </div> */}
+//       </div>
+//       <div className="index-footer">
+//         <Footer />
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Index;
+// export default Index;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,44 +1,44 @@
-import React, { Component } from "react";
-import "../stylesheets/navbar.scss";
-import { Link } from "react-router-dom";
+// import React, { Component } from "react";
+// import "../stylesheets/navbar.scss";
+// import { Link } from "react-router-dom";
 
-export default class NavBar extends Component {
-  render() {
-    return (
-      <div className="menu-wrap">
-        <input type="checkbox" className="toggler" />
-        <div className="hamburger">
-          <div></div>
-        </div>
-        <div className="menu">
-          <div>
-            <div>
-              <ul>
-                <li>
-                  <Link to="/">
-                    <a>Home</a>{" "}
-                  </Link>
-                </li>
-                {/* <li>
-                  <Link to="/work">
-                    <a>How I work</a>{" "}
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/about">
-                    <a>About me</a>{" "}
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/contact">
-                    <a>Contact details</a>
-                  </Link>
-                </li> */}
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+// export default class NavBar extends Component {
+//   render() {
+//     return (
+//       <div className="menu-wrap">
+//         <input type="checkbox" className="toggler" />
+//         <div className="hamburger">
+//           <div></div>
+//         </div>
+//         <div className="menu">
+//           <div>
+//             <div>
+//               <ul>
+//                 <li>
+//                   <Link to="/">
+//                     <a>Home</a>{" "}
+//                   </Link>
+//                 </li>
+//                 {/* <li>
+//                   <Link to="/work">
+//                     <a>How I work</a>{" "}
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/about">
+//                     <a>About me</a>{" "}
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/contact">
+//                     <a>Contact details</a>
+//                   </Link>
+//                 </li> */}
+//               </ul>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
